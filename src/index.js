452b4efce325923376835ed30bde0b76256f2afa
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2020-02-21 11:16:09 +0300

    init: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return (n + "")
+        .replace("-", "")
+        .split("")
+        .reverse()
+        .join("");
+};
